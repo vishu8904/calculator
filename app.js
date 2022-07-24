12dc999ec861624e7568f4001b48067e269a67cf
Vishwanath HS <vhs@expediagroup.com>
2022-07-24 14:28:00 +0530

    first commit on calculator service

---
+++ app.js
@@ -0,0 +1,31 @@
+async function getResult(element) {
+    console.log("getResult method called");
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let buttonId = element.id;
+    
+    const server = "https://localhost:8099/v1/"
+    const urlParams = "?num1="+num1+"&num2="+num2;
+    let url;
+    switch (buttonId) {
+        case 'add': url = server + "add" + urlParams;
+            break;
+        case 'diff': url = server + "sub" + urlParams;
+            break;
+        case 'mul': url = server + "mul" + urlParams;
+            break;
+        case 'div': url = server + "div" + urlParams;
+            break;
+    }
+    console.log("request sent to server :  " + url );
+    
+   var response = await fetch(url);
+   var output = await response.json();
+    console.log(output);
+    if (output['errors'].length > 0) {
+        document.getElementById("result").innerHTML = "Please enter valid input";
+    }
+    else {
+        document.getElementById("result").innerHTML = output['result'];
+    }
+}
